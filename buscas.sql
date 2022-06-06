4065779135e378799ad98358e66e3c8dc116028a
jaehnri <joao.henri.cr@gmail.com>
2022-06-05 23:29:45 -0300

    Commito
    Signed-off-by: jaehnri <joao.henri.cr@gmail.com>

--- buscas.sql
+++ buscas.sql
@@ -1,49 +1,68 @@
+------------------------------------------------------------------------------------------
 -- 4.1 Liste os serviços que podem ser utilizados por grupo de usuários.
-SELECT u.id, s.id
+------------------------------------------------------------------------------------------
+
+-- No caso, estamos considerando que (17, 18) corresponde ao grupo de usuários. 
+-- Também estamos considerando que queremos a união dos serviços e não a interseção.
+
+SELECT DISTINCT s.*
 FROM servico AS s, usuario AS u
 WHERE u.id IN (17, 18) AND (u.id, s.id) IN
-    (SELECT u.id AS idu, DISTINCT ps.id_servico AS ids
+    ((SELECT u.id AS idu, ps.id_servico AS ids
      FROM usuario AS u,
           rel_usuario_perfil AS up,
           rel_perfil_servico AS ps
      WHERE u.id = up.id_usuario
        AND up.id_perfil = ps.id_perfil)
    UNION
-    (SELECT t.id AS idu, DISTINCT ts.id_servico AS ids
+    (SELECT t.id AS idu, ts.id_servico AS ids
      FROM tutelado AS t,
           rel_tutela_servico AS ts
-     WHERE t.id = ts.id)
+     WHERE t.id = ts.id_tutelado));
 
+------------------------------------------------------------------------------------------
 -- 4.2) Liste em ordem crescente o total de serviços utilizados agrupados pelos tipos de
 --      serviços disponíveis e pelo perfil dos usuários.
+------------------------------------------------------------------------------------------
 
--- Uma execucao de um servico feita por um usuario com mais de umu perfil sera contada mais de uma vez, nao e possivel decidir para qual perfil uma execucao contara uma vez que esta informacao nao é mantida
+WITH perfil_servico_usado AS
+  (SELECT rup.id_perfil,
+          rus.id_servico
+   FROM rel_usuario_servico rus
+   INNER JOIN rel_usuario_perfil rup ON rus.id_usuario = rup.id_usuario
+   UNION ALL SELECT t.id_perfil,
+                    rus.id_servico
+   FROM rel_usuario_servico rus
+   INNER JOIN tutelado t ON t.id = rus.id_usuario)
+SELECT *,
+       count(*) AS frequencia
+FROM perfil_servico_usado
+GROUP BY (id_perfil,
+          id_servico)
+ORDER BY frequencia ASC,
+         id_perfil ASC,
+         id_servico ASC;
 
-SELECT us.servico,
-       up.perfil,
-       COUNT(*)
-FROM rel_usuario_servico AS us,
-     rel_usuario_perfil AS up
-WHERE us.id_usuario = up.id_usuario
-GROUP BY us.servico,
-         up.perfil
-
+------------------------------------------------------------------------------------------
 -- 4.3) Liste todos os exames realizados, com seus respectivos tipos, bem como os seus
---     usuários com suas respectivas datas de coleta de amostras.
+--      usuários com suas respectivas datas de coleta de amostras.
+------------------------------------------------------------------------------------------
 
--- Por essa pergunta eu acho q o lugar de data da amostra é msm em amostra, dai amostra deixa de ser um descritor de amostra. Se n fica muito idiota essa pergunta
+SELECT e.tipo,
+       p.nome,
+       aplicacao.data_coleta,
+       aplicacao.data_exame
+FROM rel_paciente_amostra_exame aplicacao
+INNER JOIN exame e ON e.id = aplicacao.id_exame
+INNER JOIN paciente p ON p.id = aplicacao.id_paciente;
 
-SELECT p.nome, pea.data_coleta, e.nome
-FROM rel_paciente_amostra_exame AS pae, exame AS e, paciente AS p
-WHERE pae.id_exame = e.id AND pae.id_paciente = p.id
-
+------------------------------------------------------------------------------------------
 -- 4.4) Liste os 2 exames realizados com maior frequência.
+------------------------------------------------------------------------------------------
 
-SELECT tipo
-FROM exame
-WHERE id IN
-    (SELECT id_exame AS id
-     FROM rel_paciente_amostra_exame
-     GROUP BY id_exame
-     ORDER BY COUNT(*) DESC
-     LIMIT 2);
+SELECT e.tipo, count(*) as frequencia
+FROM rel_paciente_amostra_exame aplicacao
+INNER JOIN exame e on e.id = aplicacao.id_exame
+GROUP BY e.id
+ORDER BY frequencia DESC 
+LIMIT 2;
